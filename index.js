81f193e8c75ac828310cb0fe61595b0d7edd9cc4
sarakarlssson <sara.amanda.karlsson@gmail.com>
2024-11-18 16:51:36 +0100

    Userprompt change color black
    När userprompt är samma som staden i arrayen blir bakgrundsfärgen svart

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 // Recommended: All functions declared here
+let userPrompt = prompt("Skriv en stad:")
+
+let cityNameBox;
 
 function createAllCityBoxes() {
 
     let cityNamesContainer = document.querySelector("#cities");
     for (city of cities) {
 
-        let cityNameBox = document.createElement("div");
+        cityNameBox = document.createElement("div");
         cityNameBox.className = "cityBox";
         cityNameBox.textContent = city.name;
-        cityNamesContainer.appendChild(cityNameBox)
+        cityNamesContainer.appendChild(cityNameBox);
 
+        markCityBox();
     }
-
 }
 
 createAllCityBoxes();
+
+function markCityBox() {
+
+    if (userPrompt == city.name) {
+        cityNameBox.className = "cityBox target";
+    }
+}
 
 // Recommended: constants with references to existing HTML-elements
 
