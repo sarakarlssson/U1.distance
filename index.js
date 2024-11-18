9c9a9a144ee19778c37bfc797d2c66615aa6bfda
sarakarlssson <sara.amanda.karlsson@gmail.com>
2024-11-18 15:27:11 +0100

    City Names
    Jag har skapat en for of loop för att få upp samtliga städers namn. Går igenom arrayen cities och tar ut namnen skapar sedan en div för varje stad som stylas med länkade css.

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 // Recommended: All functions declared here
 
+function createAllCityBoxes() {
+
+    let cityNamesContainer = document.querySelector("#cities");
+    for (city of cities) {
+
+        let cityNameBox = document.createElement("div");
+        cityNameBox.className = "cityBox";
+        cityNameBox.textContent = city.name;
+        cityNamesContainer.appendChild(cityNameBox)
+
+    }
+
+}
+
+createAllCityBoxes();
+
 // Recommended: constants with references to existing HTML-elements
 
 // Recommended: Ask for the city name and then the rest of the code
